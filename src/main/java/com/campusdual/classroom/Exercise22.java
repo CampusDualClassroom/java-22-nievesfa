b3276ca9eefad4b2f11bc4145b69a21ceba02a1d
nievesfa <nievesfacorro@gmail.com>
2025-01-16 20:26:46 +0100

    test ok

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,18 +7,50 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> list = new ArrayList<>();
+
+        list.add("GKFFD");
+        list.add("TNANA");
+        list.add("MPMSL");
+        list.add("PSWME");
+        list.add("LZMLF");
+        list.add("JYEBV");
+        list.add("YELNT");
+        list.add("JSNKR");
+        list.add("JFESF");
+        list.add("TMJLL");
+
+        return list;
 
     }
 
     public static void printElementsAndIndex(List<String> customList) {
+        int index = 0;
+        for (String item : customList) {
+            System.out.println("Índice: " + index + ", Valor: " + item);
+            index++;
 
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
+        return customList.add(element);
 
     }
 
+
     public static void main(String[] args) {
+        List<String> customList = createArrayList();
+        System.out.println("Lista:");
+        printElementsAndIndex(customList);
+
+        boolean added = addElementToList(customList, "AAAAA");
+        if (added){
+            System.out.println("\n Elemento añadido.");
+        }else{
+            System.out.println("\n Error.");
+        }
+        printElementsAndIndex(customList);
 
     }
 
